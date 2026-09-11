a4664485cae189094273ae83a5a41aeb9516d012
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2026-09-11 17:12:44 +0700

    Add operate function
    Create a map for mapping math operation

--- app.ts
+++ app.ts
@@ -14,6 +14,23 @@ function divide(a: number, b: number) {
   return a / b;
 }
 
-console.log(1 / 3);
+let mathOperator: string;
+let firstOperand: number;
+let secondOperand: number;
+
+function operate(mathComputation: Map<string, Function>) {
+  let operation = mathComputation.get(mathOperator);
+  return operation(firstOperand, secondOperand);
+}
+
+function main() {
+  let mathComputation = new Map<string, Function>();
+  mathComputation.set("add", add);
+  mathComputation.set("subtract", subtract);
+  mathComputation.set("multiply", multiply);
+  mathComputation.set("divide", divide);
+}
+
+main();
 
 export {};
