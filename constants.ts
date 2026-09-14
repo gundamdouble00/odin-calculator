cd263230095f0bce153cf2ab5e2ff7d553da096d
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2026-09-14 15:51:12 +0700

    Add COMPUTATIONS map and NUMBERS array
    Used for looking class name

--- constants.ts
+++ constants.ts
@@ -1,5 +1,25 @@
 export const CLICK_EVENT = "click";
 
-export const MATH_COMPUTATIONS = ["+", "-", "*", "/"];
+export const MATH_COMPUTATIONS = ["+", "-", "*", "/", "="];
+
+export const COMPUTATIONS = new Map<string, string>();
+COMPUTATIONS.set("+", "add");
+COMPUTATIONS.set("-", "subtract");
+COMPUTATIONS.set("*", "multiply");
+COMPUTATIONS.set("/", "divide");
+COMPUTATIONS.set("=", "result");
 
 export const CLEAR = "C";
+
+export const NUMBERS = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "siz",
+  "seven",
+  "eight",
+  "nine",
+];
