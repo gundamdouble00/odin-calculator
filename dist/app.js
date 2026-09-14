194dd67fd6e2b456874b820f59887e879c264fac
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2026-09-14 10:24:42 +0700

    Compiled compiled javascript code from typescript code

--- dist/app.js
+++ dist/app.js
@@ -4,31 +4,118 @@ var CLEAR = "C";
 
 // utils.ts
 function add(a, b) {
-  return a + b;
+  return Number((a + b).toFixed(2));
 }
 function subtract(a, b) {
-  return a - b;
+  return Number((a - b).toFixed(2));
 }
 function multiply(a, b) {
-  return a * b;
+  return Number((a * b).toFixed(2));
 }
 function divide(a, b) {
-  return a / b;
+  return Number((a / b).toFixed(2));
+}
+function operate(firstOperand, secondOperand, operator) {
+  if (operator === "+")
+    return add(firstOperand, secondOperand);
+  if (operator === "-")
+    return subtract(firstOperand, secondOperand);
+  if (operator === "*")
+    return multiply(firstOperand, secondOperand);
+  if (operator === "/")
+    return divide(firstOperand, secondOperand);
 }
 
 // app.ts
+var operator;
+var firstOperand;
+var secondOperand;
+var result;
 var screen = document.querySelector(".screen");
+function resetComputationElements() {
+  result = firstOperand = secondOperand = operator = undefined;
+}
 function addClearBtnEvent(clearBtn) {
   clearBtn.addEventListener(CLICK_EVENT, () => {
     if (screen != null) {
       screen.textContent = "";
     }
+    resetComputationElements();
   });
 }
 function addNumberBtnEvent(numberBtn) {
   numberBtn.addEventListener(CLICK_EVENT, () => {
-    if (screen != null) {
+    if (screen == null)
+      return;
+    if (firstOperand === undefined || result !== undefined) {
+      if (result !== undefined)
+        resetComputationElements();
+      screen.textContent = numberBtn.textContent;
+      firstOperand = Number(screen.textContent);
+      return;
+    }
+    if (firstOperand !== undefined && operator === undefined) {
       screen.textContent += numberBtn.textContent;
+      firstOperand = Number(screen.textContent);
+    }
+    if (operator !== undefined) {
+      if (secondOperand === undefined) {
+        screen.textContent = numberBtn.textContent;
+      } else {
+        screen.textContent += numberBtn.textContent;
+      }
+      secondOperand = Number(screen.textContent);
+    }
+  });
+}
+function handleResultBtn() {
+  if (firstOperand == undefined || secondOperand == undefined) {
+    return;
+  }
+  if (result === undefined && firstOperand !== undefined && secondOperand !== undefined && operator !== undefined) {
+    if (screen == null)
+      return;
+    result = operate(firstOperand, secondOperand, operator);
+    if (secondOperand === 0 && operator === "/") {
+      screen.textContent = "Cannot divide by zero";
+    } else {
+      screen.textContent = `${result}`;
+    }
+    resetComputationElements();
+  }
+}
+function addComputationBtnEvent(computationBtn) {
+  if (computationBtn.textContent === "=") {
+    computationBtn.addEventListener(CLICK_EVENT, handleResultBtn);
+    return;
+  }
+  computationBtn.addEventListener(CLICK_EVENT, () => {
+    if (firstOperand !== undefined && secondOperand === undefined) {
+      operator = computationBtn.textContent;
+      return;
+    }
+    if (screen == null)
+      return;
+    if (Number(screen.textContent) === Infinity) {
+      return;
+    }
+    if (firstOperand === undefined && secondOperand === undefined && operator === undefined && Number(screen.textContent) !== Infinity) {
+      firstOperand = Number(screen.textContent);
+      operator = computationBtn.textContent;
+      return;
+    }
+    if (firstOperand !== undefined && secondOperand !== undefined && operator !== undefined) {
+      const tempResult = operate(firstOperand, secondOperand, operator);
+      if (secondOperand === 0 && operator === "/") {
+        screen.textContent = "Cannot divide by zero";
+      } else {
+        screen.textContent = `${tempResult}`;
+      }
+      resetComputationElements();
+      if (tempResult === Infinity)
+        return;
+      firstOperand = tempResult;
+      operator = computationBtn.textContent;
     }
   });
 }
@@ -51,9 +138,7 @@ function main() {
       addNumberBtnEvent(btns[i]);
       continue;
     }
-    btns[i].addEventListener("click", () => {
-      console.log(btns[i].textContent);
-    });
+    addComputationBtnEvent(btns[i]);
   }
 }
 main();
