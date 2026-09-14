12dfe30eef72a930902886d447ea1b69ad3d3ec1
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2026-09-14 09:48:24 +0700

    Adding "operate" function

--- utils.ts
+++ utils.ts
@@ -11,5 +11,16 @@ export function multiply(a: number, b: number) {
 }
 
 export function divide(a: number, b: number) {
-  return a / b;
+  return Number((a / b).toFixed(2));
+}
+
+export function operate(
+  firstOperand: number,
+  secondOperand: number,
+  operator: string,
+) {
+  if (operator === "+") return add(firstOperand, secondOperand);
+  if (operator === "-") return subtract(firstOperand, secondOperand);
+  if (operator === "*") return multiply(firstOperand, secondOperand);
+  if (operator === "/") return divide(firstOperand, secondOperand);
 }
